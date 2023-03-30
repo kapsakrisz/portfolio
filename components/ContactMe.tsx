0bbf2d20f3ae303f80abd876f5f0941bb7d9b05c
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-03-30 09:48:06 +0100

    polish 3

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -43,7 +43,7 @@ function ContactMe({}:Props) {
                         </div>
 
                         <div className="flex items-center space-x-1 justify-center">
-                    <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"/>
+                    
                     <p className="text-2xl"></p>
                         </div>
                     </div>
